2fb5cbd6b88872e0021f7fb24ed088a0e508fc25
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2021-12-18 17:02:15 +0530

    handle login auth route

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import auth from "./models/auth.js";
+import bcrypt from "bcrypt";
+const saltRounds = 10;
 
 const app = express();
 
@@ -36,11 +38,42 @@ app.post("/auth/signup", (req, res) => {
     } else {
       if (foundEmail) {
         console.log("Rejection: used email!");
-        res.send({ error: true, errorMsg: "That email is already used!" });
+        res.json({ error: true, errorMsg: "That email is already used!" });
       } else {
-        auth.create(req.body);
-        console.log("added to the DB");
-        res.send({ error: false, msg: "Signup Successful!" });
+        bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+          auth.create({
+            userType: req.body.userType,
+            fname: req.body.fname,
+            lname: req.body.lname,
+            email: req.body.email,
+            password: hash,
+            verified: req.body.verified,
+          });
+          console.log("added new user to DB.");
+          res.json({ error: false, msg: "Signup Successful!" });
+        });
+      }
+    }
+  });
+});
+
+app.post("/auth/signin", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  auth.findOne({ email: email }, (err, foundUser) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        bcrypt.compare(password, foundUser.password, (err, result) => {
+          if (result === true) {
+            res.json({ error: false, userType: foundUser.userType });
+          } else {
+            res.json({ error: true, errorMsg: "Incorrect Password!" });
+          }
+        });
+      } else {
+        res.json({ error: true, errorMsg: "Email not registered." });
       }
     }
   });
