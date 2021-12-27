18eeb35aa6c42b6557d189524e73b6561e1ebe44
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2021-12-27 21:01:25 +0530

    modified res for errorMsg

--- middlewares/index.js
+++ middlewares/index.js
@@ -4,21 +4,25 @@ const { ACCESS_SECRET } = process.env;
 const checkAuth = (req, res, next) => {
   const token = req.get("x-auth-token");
   if (!token) {
-    return res.status(401).json({ error: "Access denied, token missing!" });
+    return res
+      .status(401)
+      .json({ error: true, errorMsg: "Access denied, token missing!" });
   } else {
     try {
       const payload = jwt.verify(token, ACCESS_SECRET);
-      req = payload; //req.user = payload.user
+      req = payload;
       next();
     } catch (error) {
       if (error.name === "TokenExpiredError") {
-        return res
-          .status(401)
-          .json({ error: "Session timed out, please login again" });
+        return res.status(401).json({
+          error: true,
+          errorMsg: "Session timed out, please login again",
+        });
       } else if (error.name === "JsonWebTokenError") {
-        return res
-          .status(401)
-          .json({ error: "Invalid token, please login again!" });
+        return res.status(401).json({
+          error: true,
+          errorMsg: "Invalid token, please login again!",
+        });
       } else {
         console.log(error);
         return res.status(400).json({ error });
