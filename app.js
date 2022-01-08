21be5c562aa64af93c75c7e912da6ef2749dbd7b
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2022-01-08 16:06:22 +0530

    modified res.sendFile path

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "public/index.html");
 });
 
 app.use("/api", api);
