bf9c2d3a995066c62e0a4d92bd2dd52ff789f1f7
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2022-01-08 15:46:23 +0530

    modified PORT

--- app.js
+++ app.js
@@ -35,10 +35,8 @@ app.get("/", (req, res) => {
 
 app.use("/api", api);
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 5000;
-}
+const port = process.env.PORT || 5000;
+
 app.listen(port, function () {
   console.log("Server started on port: ", port);
 });
