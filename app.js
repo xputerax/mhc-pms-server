085e5aec9890ee9bab28061116700d54621d8581
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2022-02-03 02:38:15 +0530

    made upload path available

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use(express.static("public"));
+app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
 
 app.use(cors());
 
