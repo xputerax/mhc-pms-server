a0f1129e378228663c59707782ee78588264cc6e
Aiman Daniel <aiman2301@gmail.com>
2023-12-20 15:57:39 +0800

    reduce nesting, add types

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,32 +1,40 @@
+import express from 'express';
 import jwt from "jsonwebtoken";
-const { ACCESS_SECRET } = process.env;
 
-const checkAuth = (req, res, next) => {
-  const token = req.get("x-auth-token");
+const { ACCESS_SECRET } = process.env;
+const TOKEN_HEADER = "x-auth-token";
+
+export interface AuthedRequest extends express.Request {
+  user?: any;
+}
+
+const checkAuth = (req: AuthedRequest, res: express.Response, next: () => void) => {
+  const token = req.get(TOKEN_HEADER);
+
   if (!token) {
     return res
       .status(401)
       .json({ error: true, errorMsg: "Access denied, token missing!" });
-  } else {
-    try {
-      const payload = jwt.verify(token, ACCESS_SECRET);
-      req = payload;
-      next();
-    } catch (error) {
-      if (error.name === "TokenExpiredError") {
-        return res.status(401).json({
-          error: true,
-          errorMsg: "Session timed out, please login again",
-        });
-      } else if (error.name === "JsonWebTokenError") {
-        return res.status(401).json({
-          error: true,
-          errorMsg: "Invalid token, please login again!",
-        });
-      } else {
-        console.log(error);
-        return res.status(400).json({ error: true, errorMsg: error });
-      }
+  }
+
+  try {
+    const payload = jwt.verify(token, ACCESS_SECRET);
+    req.user = payload;
+    next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({
+        error: true,
+        errorMsg: "Session timed out, please login again",
+      });
+    } else if (error.name === "JsonWebTokenError") {
+      return res.status(401).json({
+        error: true,
+        errorMsg: "Invalid token, please login again!",
+      });
+    } else {
+      console.log(error);
+      return res.status(400).json({ error: true, errorMsg: error });
     }
   }
 };
