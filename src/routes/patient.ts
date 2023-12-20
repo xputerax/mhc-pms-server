95c51aaf1efd135bd5f4361570f28636e24509a7
Aiman Daniel <aiman2301@gmail.com>
2023-12-20 16:01:36 +0800

    refactor: rename import, pass imported function instead of wrapping in new function

--- src/routes/patient.ts
+++ src/routes/patient.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import middleware from "../middlewares/index";
+import authMiddleware from "../middlewares/index";
 import {
   bookAppointment,
   duePayment,
@@ -13,36 +13,13 @@ import {
 
 const router = express.Router();
 
-router.post("/booking/appointment", middleware, (req, res) => {
-  bookAppointment(req, res);
-});
-
-router.get("/booking/duepayment", middleware, (req, res) => {
-  duePayment(req, res);
-});
-
-router.post("/booking/payment", middleware, (req, res) => {
-  makePayment(req, res);
-});
-
-router.get("/patient/appointments", middleware, (req, res) => {
-  myAppointments(req, res);
-});
-
-router.delete("/patient/appointments/cancel", middleware, (req, res) => {
-  cancelAppointment(req, res);
-});
-
-router.get("/patient/prescriptions", middleware, (req, res) => {
-  prescriptions(req, res);
-});
-
-router.post("/patient/feedback/write", middleware, (req, res) => {
-  writeFeedback(req, res);
-});
-
-router.post("/patient/feedback/delete", middleware, (req, res) => {
-  deleteFeedback(req, res);
-});
+router.post("/booking/appointment", authMiddleware, bookAppointment);
+router.get("/booking/duepayment", authMiddleware, duePayment);
+router.post("/booking/payment", authMiddleware, makePayment);
+router.get("/patient/appointments", authMiddleware, myAppointments);
+router.delete("/patient/appointments/cancel", authMiddleware, cancelAppointment);
+router.get("/patient/prescriptions", authMiddleware, prescriptions);
+router.post("/patient/feedback/write", authMiddleware, writeFeedback);
+router.post("/patient/feedback/delete", authMiddleware, deleteFeedback);
 
 export default router;
