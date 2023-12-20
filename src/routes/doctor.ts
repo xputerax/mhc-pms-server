e6975014849e3e9a098ba1dd620fa39c1d45eae8
Aiman Daniel <aiman2301@gmail.com>
2023-12-20 16:00:20 +0800

    refactor: pass imported function instead of creating new function

--- src/routes/doctor.ts
+++ src/routes/doctor.ts
@@ -1,16 +1,14 @@
 import express from "express";
 import { uploadPrescription } from "../controllers/doctor";
-import middleware from "../middlewares/index";
+import authMiddleware from "../middlewares/index";
 import handleFileUpload from "../middlewares/handleMulter";
 
 const router = express.Router();
 
 router.post(
   "/doctor/prescription/upload",
-  [middleware, handleFileUpload],
-  (req, res) => {
-    uploadPrescription(req, res);
-  }
+  [authMiddleware, handleFileUpload],
+  uploadPrescription
 );
 
 export default router;
