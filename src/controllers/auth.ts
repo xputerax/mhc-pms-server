a2edb05c40d84f1c45c5c87bcfc1b1b9b12b84c1
Aiman Daniel <aiman2301@gmail.com>
2023-12-20 15:51:16 +0800

    refactor auth controller
    - early return whenever possible
    - reduce nesting
    - use async/await instead of callback

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -6,38 +6,44 @@ import bcrypt from "bcrypt";
 const saltRounds = 10;
 const { ACCESS_SECRET, REFRESH_SECRET } = process.env;
 
-const signup = async (req: express.Request, res: express.Response) => {
+interface SignUpRequest extends express.Request {
+  body: {
+    userType: string,
+    password: string,
+    fname: string,
+    lname: string,
+    degree: string,
+    email: string,
+    verified: boolean,
+  }
+}
+
+const signup = async (req: SignUpRequest, res: express.Response) => {
   try {
-    auth.findOne({ email: req.body.email }, (err, foundEmail) => {
-      if (err) {
-        console.log(err);
-        return res
-          .status(500)
-          .json({ error: true, errorMsg: "Internal Server Error!" });
-      } else {
-        if (foundEmail) {
-          return res.status(400).json({
-            error: true,
-            errorMsg: "That email is already registered!",
-          });
-        } else {
-          bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-            auth.create({
-              userType: req.body.userType,
-              fname: req.body.fname,
-              lname: req.body.lname,
-              degree: req.body.degree,
-              email: req.body.email,
-              password: hash,
-              verified: req.body.verified,
-            });
-            return res
-              .status(201)
-              .json({ error: false, msg: "Signup Successful!" });
-          });
-        }
-      }
+    const foundEmail = await auth.findOne({ email: req.body.email })
+
+    if (foundEmail) {
+      return res.status(400).json({
+        error: true,
+        errorMsg: "That email is already registered!",
+      });
+    }
+
+    const hash = await bcrypt.hash(req.body.password, saltRounds)
+
+    await auth.create({
+      userType: req.body.userType,
+      fname: req.body.fname,
+      lname: req.body.lname,
+      degree: req.body.degree,
+      email: req.body.email,
+      password: hash,
+      verified: req.body.verified,
     });
+
+    return res
+      .status(201)
+      .json({ error: false, msg: "Signup Successful!" });
   } catch (error) {
     console.error(error);
     return res
@@ -46,48 +52,42 @@ const signup = async (req: express.Request, res: express.Response) => {
   }
 };
 
-const signin = (req: express.Request, res: express.Response) => {
+const signin = async (req: express.Request, res: express.Response) => {
+  const { email, password } = req.body;
+
   try {
-    const email = req.body.email;
-    const password = req.body.password;
-    auth.findOne({ email: email }, (err, foundUser) => {
-      if (err) {
-        console.log(err);
-        return res
-          .status(500)
-          .json({ error: true, errorMsg: "Internal Server Error!" });
-      } else {
-        if (foundUser && foundUser.verified) {
-          bcrypt.compare(password, foundUser.password, async (err, result) => {
-            if (result === true) {
-              const accessToken = await foundUser.createAccessToken(foundUser);
-              const refreshToken = await foundUser.createRefreshToken(
-                foundUser
-              );
-              return res.status(201).json({
-                error: false,
-                userType: foundUser.userType,
-                accessToken,
-                refreshToken,
-              });
-            } else {
-              return res
-                .status(401)
-                .json({ error: true, errorMsg: "Incorrect Password!" });
-            }
-          });
-        } else if (foundUser && !foundUser.verified) {
-          return res.status(401).json({
-            error: true,
-            errorMsg:
-              "This email is not verified by the Admin. Please login after the verification process is completed.",
-          });
-        } else {
-          return res
-            .status(404)
-            .json({ error: true, errorMsg: "Email not registered." });
-        }
-      }
+    const foundUser = await auth.findOne({ email: email })
+
+    if (!foundUser) {
+      return res
+        .status(404)
+        .json({ error: true, errorMsg: "Email not registered." });
+    }
+
+    if (!foundUser.verified) {
+      return res.status(401).json({
+        error: true,
+        errorMsg:
+          "This email is not verified by the Admin. Please login after the verification process is completed.",
+      });
+    }
+
+    const pwMatches = await bcrypt.compare(password, foundUser.password)
+
+    if (pwMatches !== true) {
+      return res
+        .status(401)
+        .json({ error: true, errorMsg: "Incorrect Password!" });
+    }
+
+    const accessToken = await foundUser.createAccessToken();
+    const refreshToken = await foundUser.createRefreshToken();
+
+    return res.status(201).json({
+      error: false,
+      userType: foundUser.userType,
+      accessToken,
+      refreshToken,
     });
   } catch (error) {
     console.error(error);
@@ -100,22 +100,25 @@ const signin = (req: express.Request, res: express.Response) => {
 const generateRefreshToken = async (req: express.Request, res: express.Response) => {
   try {
     const { refreshToken } = req.body;
+
     if (!refreshToken) {
       return res
         .status(403)
         .json({ error: true, errorMsg: "Access denied, token missing!" });
-    } else {
-      const storedToken = await token.findOne({ token: refreshToken });
-      if (!storedToken) {
-        return res
-          .status(401)
-          .json({ error: true, errorMsg: "Token Expired!" });
-      } else {
-        const payload = jwt.verify(storedToken.token, REFRESH_SECRET);
-        const accessToken = jwt.sign(payload, ACCESS_SECRET);
-        return res.status(200).json({ accessToken });
-      }
     }
+
+    const storedToken = await token.findOne({ token: refreshToken });
+
+    if (!storedToken) {
+      return res
+        .status(401)
+        .json({ error: true, errorMsg: "Token Expired!" });
+    }
+
+    const payload = jwt.verify(storedToken.token, REFRESH_SECRET);
+    const accessToken = jwt.sign(payload, ACCESS_SECRET);
+
+    return res.status(200).json({ accessToken });
   } catch (error) {
     console.error(error);
     return res
